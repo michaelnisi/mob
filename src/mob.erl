08ff9f7109ed310980d7d8737cf1d57401235b21
Michael Nisi <michael.nisi@gmail.com>
2018-03-11 12:11:26 +0100

    Hide internals

--- src/mob.erl
+++ src/mob.erl
@@ -5,6 +5,7 @@
 -export([
   start/0,
   start/2,
+  add/2,
   stop/0
 ]).
 
@@ -12,11 +13,15 @@ start() ->
   {ok, _} = application:ensure_all_started(gun),
   application:start(?MODULE).
 
-stop() ->
-  ok = application:stop(?MODULE),
-  application:stop(gun).
 
 start(Host, Port) ->
   ok = application:set_env(mob, host, Host),
   ok = application:set_env(mob, port, Port),
   start().
+
+add(StreamFun, Concurrency) ->
+  mob_sup:start_children(StreamFun, Concurrency).
+
+stop() ->
+  ok = application:stop(?MODULE),
+  application:stop(gun).
