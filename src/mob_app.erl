e99b0cb5bf48eea3ba156e30076a501176eb23c3
Michael Nisi <michael.nisi@gmail.com>
2018-03-11 18:34:07 +0100

    Remove unused function

--- src/mob_app.erl
+++ src/mob_app.erl
@@ -6,16 +6,10 @@
   stop/1
 ]).
 
-%% API
--export([go/1]).
-
 start(_Type, _Args) ->
   Host = application:get_env(mob, host, "localhost"),
   Port = application:get_env(mob, port, 8384),
   mob_sup:start_link({Host, Port}).
-
-go(StreamFun) ->
-  mob_sup:go(StreamFun).
 
 stop(_State) ->
 	ok.
